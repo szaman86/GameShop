158e47af5f9ebd61891c5b6097de1d1c3c9d62eb
Bartłomiej Marszał <bartlomiej.marszal@sbg.com.pl>
2016-03-01 21:22:43 +0100

    After adding customer after purchase we redirect to home

--- src/Controller/CustomersController.php
+++ src/Controller/CustomersController.php
@@ -139,7 +139,9 @@ class CustomersController extends AppController
             $customer = $this->Customers->patchEntity($customer, $this->request->data);
             if ($this->Customers->save($customer)) {
                 $this->Flash->success(__('The customer has been saved.'));
-                return $this->redirect(['action' => 'index']);
+                return $this->redirect(
+                    ['controller' => 'Genres', 'action' => 'customIndex']
+                );
             } else {
                 $this->Flash->error(__('The customer could not be saved. Please, try again.'));
             }
